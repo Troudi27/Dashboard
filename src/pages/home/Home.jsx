cb2d43cb58976268b7a86e9aef2e9b69d2627927
Troudi27 <troudi27troudi@gmail.com>
2024-06-11 23:19:02 +0100

    completed UI Design comp of  sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import Sidebar from "../../components/sidebar/Sidebar"
 import "./Home.scss"
 
 export default function Home() {
   return (
     <div className="home">
-        <h1 className="title">Hoooom</h1>
+        <Sidebar/>
+        <div className="homeContainer">con</div>
     </div>
   )
 }
