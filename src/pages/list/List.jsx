bc4b20806931396d17354f608a642fed99facfa7
Troudi27 <troudi27troudi@gmail.com>
2024-06-15 03:11:15 +0100

    completed UI Design DataTable

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,16 @@
 import "./List.scss"
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import DataTable from "../../components/datatable/DataTable"
 
 export default function List() {
   return (
-    <div>List</div>
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <DataTable/>
+      </div>
+    </div>
   )
 }
