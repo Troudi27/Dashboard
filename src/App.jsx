a974cd1a548bf01a73de365aac1f24ee78b47a09
Troudi27 <troudi27troudi@gmail.com>
2024-06-17 23:08:48 +0100

    completed UI Design Add new user and add new product dynamic inputs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import List from "./pages/list/List";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { productInputs, userInputs } from "./formSource";
 
 function App() {
   return (
@@ -15,7 +16,12 @@ function App() {
           <Route path="/users">
             <Route index element={<List />} />
             <Route path=":userId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route path="new" element={<New  inputs={userInputs} title="Add New User"/>} />
+          </Route>
+          <Route path="/products">
+            <Route index element={<List />} />
+            <Route path=":productd" element={<Single />} />
+            <Route path="new" element={<New  inputs={productInputs} title="Add New Product"/>} />
           </Route>
         </Routes>
       </Router>
